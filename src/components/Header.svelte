2ff32cd0389345bde98fe7bc7a1ea99e35fa1cab
khnoh <noh5524@gmail.com>
2021-04-12 19:55:08 +0900

    reset css, header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,18 @@
+<header>
+  <img src="/trello-logo.svg" alt="Trello" class="logo" />
+</header>
+
+<style>
+  header {
+    height: 40px;
+    box-sizing: border-box;
+    background-color: rgba(0, 0, 0, 0.4);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  header img.logo {
+    width: 80px;
+    opacity: 0.5;
+  }
+</style>
