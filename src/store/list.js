86b065c787d240f3030c96f4cf5130bee1600de3
khnoh <noh5524@gmail.com>
2021-04-13 20:24:44 +0900

    list store 추가

---
+++ src/store/list.js
@@ -0,0 +1,27 @@
+import { writable } from "svelte/store";
+
+const repoLists = JSON.parse(window.localStorage.getItem("lists")) || [];
+
+// _는 list.js 내부에서만 사용하는 데이터
+const _lists = writable(repoLists);
+
+// 3. _lists가 바뀌면 subscribe의 첫번째 인수 실행
+_lists.subscribe($lists => {
+  // 4. localStore에 바뀐 리스트가 저장됨
+  window.localStorage.setItem("lists", JSON.stringify($lists));
+});
+
+// custom store
+export const lists = {
+  // subscribe 참조관계 만듬 (자동 구독 형태로 만듬)
+  subscribe: _lists.subscribe,
+  add(payload) {
+    // 1. add함수가 실행되면
+    const { title } = payload;
+    // 2. _lists가 바뀌고
+    _lists.update($lists => {
+      $lists.push({ id: "", title, cards: [] });
+      return $lists;
+    });
+  }
+};
