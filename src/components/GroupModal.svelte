7bd9b1a3e62554b84bf0dcdfa04bdf5f9f598b80
khnoh <noh5524@gmail.com>
2021-05-11 21:01:11 +0900

    group 요건 정리

--- src/components/GroupModal.svelte
+++ src/components/GroupModal.svelte
@@ -2,6 +2,8 @@
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
 
+  let step = 2;
+
   const changeGroupModal = () => {
     dispatch("changeGroupModal", false);
   };
@@ -9,23 +11,49 @@
 
 <div class="group-modal">
   <div class="group-modal__header">
-    <i class="fas fa-chevron-left" />
+    <i
+      class={`fas fa-chevron-left ${step === 1 ? "invisible" : ""}`}
+      on:click={() => (step = step - 1)}
+    />
     <div class="group-modal__header__title">
       <span>Add to Workspace</span>
     </div>
     <i class="fas fa-times" on:click={changeGroupModal} />
   </div>
   <div class="group-modal__content">
-    <img
-      src="/build-team.png"
-      alt="그룹이미지"
-      class="group-modal__content__img"
-    />
-    <div class="group-modal__content__title">Build your dream team</div>
-    <div class="group-modal__content__desc">
-      Workspaces make it easy to organize boards and members in one place.
-    </div>
-    <div class="btn success group-modal__content__btn">Add to Workspace</div>
+    {#if step === 1}
+      <img
+        src="/build-team.png"
+        alt="그룹이미지"
+        class="group-modal__content__img"
+      />
+      <div class="group-modal__content__title">Build your dream team</div>
+      <div class="group-modal__content__desc">
+        Workspaces make it easy to organize boards and members in one place.
+      </div>
+      <div
+        class="btn success group-modal__content__btn"
+        on:click={() => (step = 2)}
+      >
+        Add to Workspace
+      </div>
+    {:else}
+      <div class="group-modal__content__step-2__label">Your Workspaces</div>
+      <!-- select option -> Create a new Workspace이면 step3, 
+        label - Workspace name
+        text input
+        add to workspace 버튼 disabled
+
+
+        option에서 선택했으면 addf to workspace success로 바꾸고 버튼 누르면 group 활성화
+      -->
+      <div
+        class="btn group-modal__content__btn disabled"
+        on:click={() => (step = 2)}
+      >
+        Add to Workspace
+      </div>
+    {/if}
   </div>
 </div>
 
@@ -80,6 +108,22 @@
         width: 280px;
         text-align: center;
       }
+
+      &__step-2 {
+        &__label {
+          font-weight: 700;
+          color: #5e6c84;
+          font-size: 12px;
+          line-height: 16px;
+          margin-top: 20px;
+          margin-bottom: 4px;
+          display: block;
+        }
+      }
     }
+  }
+
+  .invisible {
+    visibility: hidden;
   }
 </style>
